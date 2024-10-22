1e856cd4280817aee77d733bb8546ee4fbb3a798
Adam Langbert <adamhl@pm.me>
2024-10-22 14:59:12 -0500

    fix astro config for github pages

--- astro.config.ts
+++ astro.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://adamhl8.github.io/astro-integrations-list/",
+  site: "https://adamhl8.github.io",
+  base: "/astro-integrations-list",
 })
